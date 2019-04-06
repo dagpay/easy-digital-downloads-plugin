f6df2631adf70f4ccd16382cdb14c8b344f8c87c
Rene Aavik <renx81@gmail.com>
2019-04-06 13:16:19 +0300

    Add hooks for registering gateway and settings section

--- src/Plugin.php
+++ src/Plugin.php
@@ -9,11 +9,24 @@ final class Plugin
         $this->registerHooks();
     }
 
+    /**
+     * Register plugin actions and filters.
+     */
     private function registerHooks()
     {
+        // load plugin text domain
         add_action('plugins_loaded', [$this, 'actionLoadTextDomain']);
+
+        // easy digital downloads hooks
+        add_filter('edd_payment_gateways', [$this, 'filterRegisterGateway']);
+        add_action('edd_dagpay_cc_form', '__return_false');
+        add_filter('edd_settings_sections_gateways', [$this, 'filterRegisterGatewaySettingsSection']);
+        add_filter('edd_settings_gateways', [$this, 'filterRegisterGatewaySettings']);
     }
 
+    /**
+     * Load plugin text domain.
+     */
     public function actionLoadTextDomain()
     {
         load_plugin_textdomain(
@@ -21,5 +34,43 @@ final class Plugin
             false,
             ULTRALEET_DAGPAY_EDD_LANGUAGES_PATH
         );
+    }
+
+    /**
+     * Register payment gateway.
+     *
+     * @param array $gateways
+     * @return array
+     */
+    public function filterRegisterGateway(array $gateways): array
+    {
+        $label = __('Dagpay', 'dagpay-edd');
+        $gateways['dagpay'] = ['admin_label' => $label, 'checkout_label' => $label];
+        return $gateways;
+    }
+
+    /**
+     * Add section to payment gateway settings.
+     *
+     * @param array $gatewaySections
+     * @return array
+     */
+    public function filterRegisterGatewaySettingsSection(array $gatewaySections): array
+    {
+        $gatewaySections['dagpay'] = __('Dagpay', 'dagpay-edd');
+        return $gatewaySections;
+    }
+
+    public function filterRegisterGatewaySettings(array $gatewaySettings): array
+    {
+        $settings = [
+            'dagpay_settings' => [
+                'id' => 'dagpay_settings',
+                'name' => '<strong>' . __('Dagpay Settings', 'dagpay-edd') . '</strong>',
+                'type' => 'header',
+            ],
+        ];
+        $gatewaySettings['dagpay'] = $settings;
+        return $gatewaySettings;
     }
 }
